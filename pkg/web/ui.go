86719a0ad8ca09ee04d8c074d5d0572d0d331841
Julien Sellier <juju.sel.mail@gmail.com>
2023-06-16 00:10:08 +0200

    add parse html template from fs

--- pkg/web/ui.go
+++ pkg/web/ui.go
@@ -2,11 +2,16 @@ package web
 
 import (
 	"html/template"
+	"io/fs"
 	"net/http"
 )
 
 func MustParseHTMLTemplate(commonTmpls []string, path string) *template.Template {
 	return template.Must(template.ParseFiles(append(commonTmpls, path)...))
+}
+
+func MustParseHTMLTemplateFS(fsys fs.FS, commonTmpls []string, path string) *template.Template {
+	return template.Must(template.ParseFS(fsys, append(commonTmpls, path)...))
 }
 
 // Will panic when an error occurs during rendering, make sure you handle panic recovery in a middleware.
